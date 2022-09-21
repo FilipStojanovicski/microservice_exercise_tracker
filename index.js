4f1bba223aa692bf1a7c137bfa186a3150bdeef4
Filip Stoyanovic <filipstoyanovic@Filips-MacBook-Pro-2.local>
2022-09-21 15:36:42 -0400

    stuff

--- index.js
+++ index.js
@@ -24,6 +24,15 @@ const userSchema = new Schema({
 })
 let userModel = mongoose.model("user", userSchema);
 
+// Exercise
+const exerciseSchema = new Schema({
+  _id: {type: String, required: true},
+  description: {type: String, required: true},
+  duration: {type: Number, required: true},
+  date: {type: Date, default: new Date()}
+})
+let exerciseModel = mongoose.model("exercise", exerciseSchema);
+
 app.use(cors())
 app.use(express.static('public'))
 
@@ -49,9 +58,32 @@ app.get('/api/users', function(req, res){
   )
 })
 
+app.post('/api/users/:_id/exercises', function(req, res){
+  let userID = req.params._id;
+  console.log(userID);
+  console.log(req.body);
 
+  exerciseObj = {
+    _id: userID,
+    description: req.body.description,
+    duration: req.body.duration
+  }
 
+  if (req.body.date != ''){
+    exerciseObj.date = req.body.date
+  }
 
+  let newExercise = new exerciseModel(exerciseObj);
+  userModel.findById(userID, function (err, userFound){
+    if (err) return console.log(err);
+    console.log(userFound);
+    console.log(exerciseObj);
+    res.json({_id: userFound.userID, username: userFound.username,
+      description: newExercise.description, duration: newExercise.duration,
+      date: new Date(newExercise.date).toDateString()
+    });
+  });
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
